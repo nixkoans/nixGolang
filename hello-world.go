d040fbc5070f15d3a56aa7918905b393f5453e44
Calvin Cheng <linchuan.cheng@gmail.com>
2015-05-27 13:56:53 +0000

    pty is now available for import in our go source, thanks to nix packaging

--- hello-world.go
+++ hello-world.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/kr/pty"
+	"io"
+	"os"
+	"os/exec"
+)
 
 func main() {
 	fmt.Println("hello world")
+
+	c := exec.Command("grep", "--color=auto", "bar")
+	f, err := pty.Start(c)
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		f.Write([]byte("foo\n"))
+		f.Write([]byte("bar\n"))
+		f.Write([]byte("baz\n"))
+		f.Write([]byte{4})
+	}()
+	io.Copy(os.Stdout, f)
 }
